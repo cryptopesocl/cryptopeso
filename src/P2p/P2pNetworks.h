5af299750e131abbe959ee6fc2c7aecfcab6f1c6
Crypto Peso Team <apoyo@cryptopeso.tk>
2017-12-26 05:18:40 -0300

    Added parameters
    missing seed ip

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x54, 0x3, 0x14, 0x5a, 0x43, 0x46, 0x4e, 0x3c, 0x11, 0x1b, 0x00, 0x10, 0x10, 0x5b, 0x9, 0x44 } };
+}
